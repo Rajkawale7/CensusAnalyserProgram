4116ccea68c8a60e668119b8528ef645b42011eb
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-26 14:23:32 +0530

    Added: Program to Evaluate the census of india

---
+++ src/main/java/com/bridgelabz/ExpectedException.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class ExpectedException {
+    public static ExpectedException none() {
+        return null;
+    }
+
+    public void expect(Class<CensusAnalyserException> censusAnalyserExceptionClass) {
+        return;
+    }
+}
